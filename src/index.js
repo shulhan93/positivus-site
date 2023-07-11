c3618ed7f6d41c60b3cb0e155fc11ba0db602e5d
shulhan93 <shulhan.andrei@gmail.com>
2023-07-11 09:56:26 +0300

    section process done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './sass/main.scss';
 
-console.log('Hello Andrei');
+const processContainer = document.querySelector('.process__inner');
+
+processContainer.addEventListener('click', (e) => {
+  const process = e.target.closest('.process__item');
+
+  if (!process) return;
+
+  Array.from(processContainer.children).forEach((element) => {
+    element.classList.remove('open');
+  });
+  process.classList.toggle('open');
+});
